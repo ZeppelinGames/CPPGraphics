a3c7786953a22590eda85e2dc3b46b3d18bd06da
Zeppelin Games <zeppelingames3@gmail.com>
2021-01-14 14:16:21 +1100

    Circle performance increased
    Removed circle drawing overlap
    Added point drawing
    Added FPS counter

--- CPPGraphics/Engine.cpp
+++ CPPGraphics/Engine.cpp
@@ -1,5 +1,8 @@
 #include <Windows.h>
 #include <math.h>
+#include <iostream>
+#include <string>
+#include <ctime>
 #include "Bounds.h"
 #include "Mathf.h"
 #include "Vector2.h"
@@ -10,6 +13,8 @@ bool running = true;
 
 int halfWidth;
 int halfHeight;
+
+int fps=0;
 
 struct Render_State {
 	int width, height;
@@ -62,6 +67,30 @@ LRESULT CALLBACK Window_Callback(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wPa
 	return result;
 }
 
+std::wstring StringToWString(const std::string& s)
+{
+	int len;
+	int slength = (int)s.length() + 1;
+	len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
+	wchar_t* buf = new wchar_t[len];
+	MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
+	std::wstring r(buf);
+	delete[] buf;
+	return r;
+}
+
+double clockToMilliseconds(clock_t ticks) {
+	// units/(units/time) => time (seconds) * 1000 = milliseconds
+	return (ticks / (double)CLOCKS_PER_SEC) * 1000.0;
+}
+
+clock_t deltaTime = 0;
+unsigned int frames = 0;
+double  frameRate = 30;
+double  averageFrameTimeMilliseconds = 33.333;
+std::wstring conv = L"0";
+
+
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
 	//Create Window Class
 	WNDCLASS window_class = {};
@@ -73,9 +102,9 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	RegisterClass(&window_class);
 
 	//Create window
-	HWND window = CreateWindow(window_class.lpszClassName, L"Main Window", WS_SYSMENU | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1920, 1080, 0, 0, hInstance, 0);
+	HWND window = CreateWindow(window_class.lpszClassName, L"Main Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1920, 1080, 0, 0, hInstance, 0);
 	HDC hdc = GetDC(window);
-	
+
 	//Get monitor
 	MONITORINFO monitor_info;
 	monitor_info.cbSize = sizeof(monitor_info);
@@ -85,7 +114,10 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	RECT window_rect(monitor_info.rcMonitor);
 
 	float angle = 0;
+
 	while (running) {
+		clock_t beginFrame = clock();
+
 		//Input
 		MSG message;
 		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
@@ -95,19 +127,44 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 		//Simulate
 		ClearScreen(0x000000);
-		//RenderBackground();
-		DrawRect(Vector2(10,200), Vector2(100, 100), 0xFFFFFF);
 
-		DrawRotatedRect(Vector2(700, 25), Vector2(100, 100),angle, 0xFFFFFF);
+		//RenderBackground();
+		
+		DrawRect(Vector2(10, 200), Vector2(100, 100), 0xFFFFFF);
+
+		DrawRotatedRect(Vector2(700, 25), Vector2(100, 100), angle, 0xFFFFFF);
 		angle += 1.0f;
 
-		DrawLine(Vector2(200, 300), Vector2(200, 600), 0xFFFFFF);
-		DrawLine(Vector2(200, 600), Vector2(600, 600), 0xFFFFFF);
-		DrawLine(Vector2(-100, 600), Vector2(900, 200), 0xFFFFFF);
+		//DrawLine(Vector2(200, 300), Vector2(200, 600), 0xFFFFFF);
+		//DrawLine(Vector2(200, 600), Vector2(600, 600), 0xFFFFFF);
+		//DrawLine(Vector2(-100, 600), Vector2(900, 200), 0xFFFFFF);
 
-		DrawCircle(Vector2(800, 800), 300, 0xFFFFFF);
-		
+		Point(Vector2(halfWidth, halfHeight), 0xFFFFFF);
+
+		DrawThickLine(Vector2(1000, 500), Vector2(800, 700), 100, 0xFFFFFF);
+
+		//DrawCircle(Vector2(800, 800), 300, 0xFFFFFF);
+		//DrawCircle(Vector2(900, 600), 100, 5, 0xFFFFFF);
+		//DrawCircle(Vector2(700, 600), 100, true, 0xFFFFFFF); 
+
 		//Render 
 		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+
+		clock_t endFrame = clock();
+		deltaTime += endFrame - beginFrame;
+		frames++;
+
+		if (clockToMilliseconds(deltaTime) > 1000.0) { //every second
+			frameRate = (double)frames * 0.5 + frameRate * 0.5; //more stable
+			frames = 0;
+			deltaTime -= CLOCKS_PER_SEC;
+			averageFrameTimeMilliseconds = 1000.0 / (frameRate == 0 ? 0.001 : frameRate);
+		}
+
+		//Draw FPS on screen
+		RECT rect = { 20,20, 200, 200 };
+		conv = StringToWString(std::to_string((int)frameRate)); //Convert string to WString
+		LPCWSTR text = conv.c_str(); //Convert WString to LPCWSTR
+		DrawText(hdc, text, -1, &rect, DT_LEFT); //Draw Text
 	}
 };
--- CPPGraphics/Renderer.cpp
+++ CPPGraphics/Renderer.cpp
@@ -18,53 +18,9 @@ void ClearScreen(unsigned int col) {
 	}
 }
 
-void DrawThickLine(Vector2 p1, Vector2 p2,int thickness, unsigned int color) {
-	int x0 = Mathf::Clamp(p1.x, 1, render_state.width - 1);
-	int x1 = Mathf::Clamp(p2.x, 1, render_state.width - 1);
-
-	int y0 = Mathf::Clamp(p1.y, 1, render_state.height - 1);
-	int y1 = Mathf::Clamp(p2.y, 1, render_state.height - 1);
-
-	if (x0 == x1) {
-		//Straight up (no gradient)
-		int from = (y0 <= y1) ? y0 : y1;
-		int to = (y0 < y1) ? y1 : y0;
-
-		for (int y = from; y < to; y++) {
-			for (int x = -thickness; x < thickness; x++) {
-				unsigned int* pixel = (unsigned int*)render_state.memory + (x0 + x) + y * render_state.width;
-				*pixel++ = color;
-			}
-		}
-	}
-	else if (y0 == y1) {
-		//Horizontal line (m=0)
-		int from = (x0 <= x1) ? x0 : x1;
-		int to = (x0 < x1) ? x1 : x0;
-
-		for (int x = from; x < to; x++) {
-			for (int y = -thickness; y < thickness; y++) {
-				unsigned int* pixel = (unsigned int*)render_state.memory + x + (y0 + y) * render_state.width;
-				*pixel++ = color;
-			}
-		}
-	}
-	else {
-		int dx = x1 - x0;
-		int dy = y1 - y0;
-
-		int from = (x0 <= x1) ? x0 : x1;
-		int to = (x0 < x1) ? x1 : x0;
-
-		for (int x = from; x < to; x++) {
-			int y = y1 + dy * (x - x1) / dx;
-
-			unsigned int* pixel = (unsigned int*)render_state.memory + (x+thickness)+(y+thickness)*render_state.width;
-			for (int c = -thickness; c < thickness; c++) {
-				*pixel++ = color;
-			}
-		}
-	}
+void Point(Vector2 pos, unsigned int color) {
+	unsigned int* pixel = (unsigned int*)render_state.memory + (int)pos.x + (int)pos.y * render_state.width;
+	*pixel++ = color;
 }
 
 void DrawLine(Vector2 p1, Vector2 p2, unsigned int color) {
@@ -111,7 +67,7 @@ void DrawLine(Vector2 p1, Vector2 p2, unsigned int color) {
 		int to = (x0 < x1) ? x1 : x0;
 
 		for (int x = from; x < to; x++) {
-			int y = y1 + dy * (x - x1) / dx;
+			float y = y1 + dy * (x - x1) / dx;
 
 			int drawX = x;
 			int drawY = y;
@@ -122,6 +78,77 @@ void DrawLine(Vector2 p1, Vector2 p2, unsigned int color) {
 		}
 	}
 }
+
+void DrawThickLine(Vector2 p1, Vector2 p2,int thickness, unsigned int color) {
+	int x0 = Mathf::Clamp(p1.x, 1, render_state.width - 1);
+	int x1 = Mathf::Clamp(p2.x, 1, render_state.width - 1);
+
+	int y0 = Mathf::Clamp(p1.y, 1, render_state.height - 1);
+	int y1 = Mathf::Clamp(p2.y, 1, render_state.height - 1);
+
+	if (x0 == x1) {
+		//Straight up (no gradient)
+		int from = (y0 <= y1) ? y0 : y1;
+		int to = (y0 < y1) ? y1 : y0;
+
+		for (int y = from; y < to; y++) {
+			for (int x = -thickness; x < thickness; x++) {
+				unsigned int* pixel = (unsigned int*)render_state.memory + (x0 + x) + y * render_state.width;
+				*pixel++ = color;
+			}
+		}
+	}
+	else if (y0 == y1) {
+		//Horizontal line (m=0)
+		int from = (x0 <= x1) ? x0 : x1;
+		int to = (x0 < x1) ? x1 : x0;
+
+		for (int x = from; x < to; x++) {
+			for (int y = -thickness; y < thickness; y++) {
+				unsigned int* pixel = (unsigned int*)render_state.memory + x + (y0 + y) * render_state.width;
+				*pixel++ = color;
+			}
+		}
+	}
+	else {
+		int dx = x1 - x0;
+		int dy = y1 - y0;
+
+		int from = (x0 <= x1) ? x0 : x1;
+		int to = (x0 < x1) ? x1 : x0;
+		
+
+		//Draw boxed
+		DrawLine(p1, p2, color);
+		
+		//get side points
+
+		//find angle between points
+		//find hyp, sine rule, find top angle, minus 180 from top
+
+		float hyp = sqrt(pow(dx, 2) + pow(dy, 2));
+		float angle = asin((sin(90) / hyp) * dx);
+		float adjAngle = abs(angle - 90);
+		Vector2 topUp = Vector2(cos(adjAngle) *thickness, sin(adjAngle) * thickness);
+		DrawLine(p1, topUp + p1,color);
+		DrawLine(p2, topUp + p2, color);
+
+		adjAngle = abs(angle + 90);
+		Vector2 topDown = Vector2(cos(adjAngle) * thickness, sin(adjAngle) * thickness);
+		DrawLine(p1, topDown + p1, color);
+		DrawLine(p2, topDown + p2, color);
+
+		/*for (int x = from; x < to; x++) {
+			int y = y1 + dy * (x - x1) / dx;
+
+			unsigned int* pixel = (unsigned int*)render_state.memory + (x+thickness)+(y+thickness)*render_state.width;
+			for (float c = -thickness; c < thickness; c++) {
+				*pixel++ = color;
+			}
+		}*/
+	}
+}
+
 
 void DrawRect(Vector2 position, Vector2 size, unsigned int color) {
 	int x0 = Mathf::Clamp(position.x - size.x, 1, render_state.width-1);
@@ -177,16 +204,69 @@ void DrawRotatedRect(Vector2 position, Vector2 size, float rotation, unsigned in
 	DrawLine(newBL, newTL, color);
 }
 
-void DrawCircle(Vector2 position, float radius, unsigned int color) {
-	for (float r = 0; r < 360; r += (M_PI / radius)) {
+void DrawCircle(Vector2 position, float radius, bool filled, unsigned int color) {
+	Vector2 previousPoint = Vector2(cos(1) * radius, sin(1) * radius);
+	
+	for (float r = 0; r < (M_PI * 2); r += (M_PI / radius)) {
 		int x = cos(r) * radius;
 		int y = sin(r) * radius;
 
 		int drawX = position.x + x;
 		int drawY = position.y + y;
-		if (Bounds::pointInBounds(Vector2(drawX,drawY), Bounds(Vector2(halfWidth, halfHeight),Vector2(halfWidth-1,halfHeight-1)))) {
-			unsigned int* pixel = (unsigned int*)render_state.memory + drawX + drawY * render_state.width;
-			*pixel++ = color;
-		}
+
+		DrawLine(Vector2(drawX, drawY), previousPoint, color);
+		previousPoint = Vector2(drawX, drawY);
+	}
+
+	if (filled) {
 	}
 }
+
+void DrawCircle(Vector2 position, float radius, int edgeRadius, bool filled, unsigned int color) {
+	Vector2 previousPoint = Vector2(cos(1) * radius, sin(1) * radius);
+
+	for (float r = 0; r < (M_PI * 2); r += (M_PI / radius)) {
+		int x = cos(r) * radius;
+		int y = sin(r) * radius;
+
+		int drawX = position.x + x;
+		int drawY = position.y + y;
+
+		DrawThickLine(Vector2(drawX, drawY), previousPoint, edgeRadius, color);
+		previousPoint = Vector2(drawX, drawY);
+	}
+
+	if (filled) {
+	}
+}
+
+void DrawCircle(Vector2 position, float radius, int edgeRadius, unsigned int color) {
+	Vector2 previousPoint = Vector2(cos(1) * radius, sin(1) * radius);
+
+	for (float r = 0; r < (M_PI * 2); r += (M_PI / radius)) {
+		int x = cos(r) * radius;
+		int y = sin(r) * radius;
+
+		int drawX = position.x + x;
+		int drawY = position.y + y;
+
+		DrawThickLine(Vector2(drawX, drawY), previousPoint, edgeRadius, color);
+		previousPoint = Vector2(drawX, drawY);
+	}
+}
+
+void DrawCircle(Vector2 position, float radius, unsigned int color) {
+	Vector2 previousPoint = Vector2(cos(0) * radius, sin(0) * radius);
+
+	for (float r = 0; r < (M_PI * 2); r += (M_PI / radius)) {
+		int x = cos(r) * radius;
+		int y = sin(r) * radius;
+
+		int drawX = position.x + x;
+		int drawY = position.y + y;
+
+		DrawLine(Vector2(drawX, drawY), previousPoint, color);
+		previousPoint = Vector2(drawX, drawY);
+	}
+}
+
